3cf37500e0638b8bf77d47fd03d434d4e991379a
Josue Vicente <josue.vicente@ufl.edu>
2024-02-05 19:21:12 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Countdown from 'react-countdown';
+
+function App() {
+  return (
+    <div className="center-screen">
+        <p className="title"> Time Until Code For Change </p>
+        <Countdown className="timer" date={"2024-02-10T09:00:00"} />
+
+    </div>
+  );
+}
+
+export default App;
